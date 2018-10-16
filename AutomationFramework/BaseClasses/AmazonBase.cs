a9123ea8ba7979e0fe7a798d9b44282099fefd48
Aniket <anikets160@gmail.com>
2018-10-16 22:26:33 +0530

    Update AutomationFramework/BaseClasses/AmazonBase.cs

--- AutomationFramework/BaseClasses/AmazonBase.cs
+++ AutomationFramework/BaseClasses/AmazonBase.cs
@@ -22,6 +22,13 @@ namespace AutomationFramework.BaseClasses
         public static readonly log4net.ILog log = log = log4net.LogManager.GetLogger("Base");
         public static WebDriverWait wait;
 
+        //enum Browser
+        //{
+        //    chrome,
+        //    firefox,
+        //    ie,
+        //    safari
+        //}
 
         public void Initialize()
         {
