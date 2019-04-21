2934f361fe9bb6fcf4799fff21a7e2e62b1338ca
Aniket <anikets160@gmail.com>
2019-04-21 22:14:03 +0530

    C
    C

--- PartialClassConcept/Program.cs
+++ PartialClassConcept/Program.cs
@@ -11,11 +11,13 @@ namespace PartialClassConcept
         static void Main(string[] args)
         {
             Employee employee = new Employee(101, "Aniket", 1600000);
+
             employee.PrintDetails();
 <<<<<<< HEAD
             //employee.PrintSalary();
             //Console.ReadKey();
 =======
+
         }
     }
 }
