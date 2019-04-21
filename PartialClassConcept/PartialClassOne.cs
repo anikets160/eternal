98c612a8b65fc77ecacdb8c88222e216219da4ae
Aniket <anikets160@gmail.com>
2019-04-21 21:42:18 +0530

    Collections and Partial Class
    Collections and Partial Class

---
+++ PartialClassConcept/PartialClassOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassConcept
+{
+    public partial class Employee
+    {
+        private int _id;
+        private string _name;
+
+        public Employee(int Id,string Name,int Salary)
+        {
+            _id = Id;
+            _name = Name;
+            _salary = Salary;
+        }
+       
+
+    }
+}
